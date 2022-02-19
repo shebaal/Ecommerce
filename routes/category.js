110a82bc4f4bbbea9d255a345388300c567aa84f
shebaal <79754297+shebaal@users.noreply.github.com>
2022-02-19 13:06:22 +0300

    edit some peoblems

--- routes/category.js
+++ routes/category.js
@@ -1,21 +1,17 @@
-// const { default: axios } = require('axios');
-const { response } = require('express');
 var express = require('express');
 var router = express.Router();
-const axios=require('axios');
-const url ='https://dummyjson.com/products' 
 
-/* GET home page. */
+const axios=require('axios');
+
+// categories
 router.get('/', function(req, res, next) {
 
   axios.get('https://jsonplaceholder.typicode.com/posts')
   .then((response)=>{
-    res.render('index', { pposts: response.data});
+    res.render('category', { pposts: response.data});
   }).catch((err)=>{
     console.log("opps ther is no data")
   })
 });
 
-
 module.exports = router;
- 
