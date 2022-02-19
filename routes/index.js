e45b7f517049352696676aff99b9025b77bc67c6
shebaal <79754297+shebaal@users.noreply.github.com>
2022-02-19 11:43:19 +0300

    add card  desgin

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// const { default: axios } = require('axios');
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const axios=require('axios');
+const url ='https://dummyjson.com/products' 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+});
+
+module.exports = router;
+ 
