0d6d6ee8b43a1699baf9beb87702784f414b9de0
shebaal <79754297+shebaal@users.noreply.github.com>
2022-02-19 11:52:56 +0300

    add for loop for cards

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+// const { default: axios } = require('axios');
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const axios=require('axios');
+const url ='https://dummyjson.com/products' 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then((response)=>{
+    res.render('index', { pposts: response.data});
+  }).catch((err)=>{
+    console.log("opps ther is no data")
+  })
+});
+
+module.exports = router;
+ 
